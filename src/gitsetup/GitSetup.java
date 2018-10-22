c4496c6529f2f990001b037331fc64d05a4524cd
sabbott0702@gmail.com <>
2018-10-22 15:54:10 -0400

    Thie is the first commit

---
+++ src/gitsetup/GitSetup.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitsetup;
+
+/**
+ *
+ * @author sma25
+ */
+public class GitSetup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Abbott();
+    }
+    
+    public static void Abbott(){
+        System.out.println("Sean Abbott");
+    }
+    
+}
